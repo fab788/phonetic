8416be4cdccf9d0f9efaa3f1978e22f1319eee22
fab788 <kelly5831036@gmail.com>
2024-06-24 16:49:44 +0800

    phonetic.py

--- phonetic.py
+++ phonetic.py
@@ -1,19 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-#word = input( '請輸入中文字:' )
+#word = input( '請輸入詩人名稱:' )
 def read(word):
-    url = f'https://dict.revised.moe.edu.tw/search.jsp?md=1&word={word}#searchL'
+    url = f'https://fanti.dugushici.com/ancient_proses/query?utf8=✓&q%5Bauthor_name_cont%5D={word}&q%5Btitle_cont%5D=&commit=搜++尋'
 
     html = requests.get( url )
     bs = BeautifulSoup(html.text,'lxml')
-    data = bs.find('table', id='searchL')
+    data = bs.find('table',width="100%")
     try:
-        row = data.find_all('tr')[2]
-        chinese = row.find('cr').text
-        phones = row.find_all('code')
-        phone = [e.text for e in phones]
-        s = " ".join( phone )
-        # s = row.find('sub')
-        return chinese +'=>'+s 
+        row = data.find_all('tr')[1]
+        poem = row.find('td').text
+        return '作品:' + poem 
     except:
-        return '查無此字' 
+        return '查無此人' 
